cd40c25ce68d0297ba91029f480f30e3fc0b1cf4
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-10-14 12:07:37 +0530

    Navbar Updated

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { Link } from 'react-scroll';
+import { FaFacebookF, FaInstagram } from 'react-icons/fa'; // Import icons
 import './Navbar.css';
 
-const Navbar = ({ toggleTheme }) => {
+const Navbar = () => {
   return (
     <motion.nav
       className="navbar"
@@ -11,6 +12,7 @@ const Navbar = ({ toggleTheme }) => {
       animate={{ y: 0 }}
       transition={{ type: 'spring', stiffness: 120 }}
     >
+   
       <div className="logo">Punjabi Tadka</div>
       <ul className="nav-links">
         <li><Link to="home" smooth={true} duration={500}>Home</Link></li>
@@ -19,14 +21,22 @@ const Navbar = ({ toggleTheme }) => {
         <li><Link to="reviews" smooth={true} duration={500}>Reviews</Link></li>
         <li><Link to="contact" smooth={true} duration={500}>Contact</Link></li>
       </ul>
-      <button className="theme-toggle" onClick={toggleTheme}>
-        🌞 Lighten Up!
-      </button>
+
+      {/* Social Media Icons */}
+      <div className="social-icons">
+        <a href="https://www.instagram.com" target="_blank" rel="noreferrer">
+          <FaInstagram className="social-icon" />
+        </a>
+        <a href="https://www.facebook.com" target="_blank" rel="noreferrer">
+          <FaFacebookF className="social-icon" />
+        </a>
+      </div>
     </motion.nav>
   );
 };
 
 export default Navbar;
+
 
 
 
