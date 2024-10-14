92cd663a8524ac947a60d2a3e10ce607ed15f421
heyAnkit18 <ankitkumarjune18@gmail.com>
2024-10-14 11:41:46 +0530

    Footer.jsx Added

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,10 +1,26 @@
-
-import React from 'react'
+import React from 'react';
+import './Footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <footer className="footer">
+      <div className="footer-content">
+        <div className="footer-left">
+          <h3>Punjabi tadka</h3>
+          <p>&copy; {new Date().getFullYear()} All rights reserved.</p>
+        </div>
 
-export default Footer
+        <div className="footer-center">
+          <h4>Contact Us</h4>
+          <p>Email: support@company.com</p>
+          <p>Phone: +123 456 7890</p>
+          <p>Address: 123, Street Name, Mumbai, Inida</p>
+        
+          </div>
+        </div>
+      
+    </footer>
+  );
+};
+
+export default Footer;
